56adab374483b871e38683077e46a797f52d1294
LMEE <42374804+lmdouble@users.noreply.github.com>
2019-01-24 14:37:27 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+
+<html>
+<head>
+<?php
+echo '<title>欢迎访问我的网站</title>';
+?>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+<body>
+<p><?php
+
+echo "welcome to visite my website";
+
+?>
+</p>
+<br />
+<a href="<?php echo 'https://www.lmdouble.com' ; ?>" >www.lmdouble.com</a>
+</body>
+</html>
